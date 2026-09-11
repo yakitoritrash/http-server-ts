cf47b5ce5a5cdad2b079b86b9c78500379d872e9
Arch me <kishudwivedi30@gmail.com>
2026-09-12 04:37:17 +0530

    server init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello, World!");
+import express, { type Express, type Request, type Response } from 'express';
+
+const app: Express = express();
+const port = 8080;
+
+app.listen(port, () => {
+  console.log(`Hello listening on port: ${port}`);
+})
+
